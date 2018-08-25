f63afa73c36bfdeabaf617f811c8b00f663a0ad1
Roman Gherta <roman.gherta@gmail.com>
2018-08-26 02:29:55 +0300

    first commit

---
+++ app/main.js
@@ -0,0 +1,23 @@
+const electron = require('electron');
+const { shell, app, BrowserWindow } = electron;
+const HOMEPAGE = 'https://www.netflix.com/'
+
+let mainWindow;
+
+app.on('ready', () => {
+    window = new BrowserWindow({
+        width: 1200,
+        height: 900,
+        icon: './icon.png',
+        webPreferences: {
+          nodeIntegration: false
+        }
+    });
+    window.setMenuBarVisibility(false);
+    window.loadURL(HOMEPAGE);
+
+
+    window.on('closed', () => {
+        window = null;
+    });
+});
