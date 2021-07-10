d33f2ee5df238c659307aa143d42c7432c382a92
romh <romh@dtu.dk>
2021-07-10 13:53:18 +0200

    devel 0.9.0

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ const { app, BrowserView, BrowserWindow } = electron;
 const HOMEPAGE = 'https://www.netflix.com/';
 
 
-    app.on('widevine-ready', () => {
+    app.on('widevine-ready', (version, lastVersion) => {
+
+        if (null !== lastVersion) {
+            console.log('Widevine ' + version + ', upgraded from ' + lastVersion + ', is ready to be used!')
+        } else {
+            console.log('Widevine ' + version + ' is ready to be used!')
+        }
+
+
         window = new BrowserWindow({
             width: 1200,
             height: 900,
@@ -15,6 +23,7 @@ const HOMEPAGE = 'https://www.netflix.com/';
             },
             plugins: true
         });
+
         window.setMenuBarVisibility(false);
         window.loadURL(HOMEPAGE);
     
@@ -22,15 +31,21 @@ const HOMEPAGE = 'https://www.netflix.com/';
     
         window.setBrowserView(view);
     
-    
         window.on('closed', () => {
             window = null;
         });
+
     })
 
 
   
+    app.on('widevine-update-pending', (currentVersion, pendingVersion) => {
+        console.log('Widevine ' + currentVersion + ' is ready to be upgraded to ' + pendingVersion + '!')
+    })
 
 
-
+    app.on('widevine-error', (error) => {
+        console.log('Widevine installation encountered an error: ' + error)
+        process.exit(1)
+    })
 
